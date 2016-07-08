8593f2a738814d76d46ebe333d5c0ecfb1a69f4a
geekarist <christophe.pele@gmail.com>
2016-07-08 07:49:09 +0200

    Implement RecyclerView in ListContentFragment

--- 1-Base/src/main/java/com/example/android/materialdesigncodelab/ListContentFragment.java
+++ 1-Base/src/main/java/com/example/android/materialdesigncodelab/ListContentFragment.java
@@ -3,9 +3,14 @@ package com.example.android.materialdesigncodelab;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -13,17 +18,49 @@ import android.view.ViewGroup;
  */
 public class ListContentFragment extends Fragment {
 
-
     public ListContentFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_content, container, false);
+        RecyclerView recyclerView = (RecyclerView) inflater.inflate(R.layout.recycler_view, container, false);
+        ContentAdapter adapter = new ContentAdapter();
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        return recyclerView;
     }
 
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.fragment_list_content, parent, false));
+        }
+    }
+
+    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
+
+        public static final int SIZE = 18;
+
+        public ContentAdapter() {
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+            return new ViewHolder(inflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+        }
+
+        @Override
+        public int getItemCount() {
+            return SIZE;
+        }
+    }
 }
