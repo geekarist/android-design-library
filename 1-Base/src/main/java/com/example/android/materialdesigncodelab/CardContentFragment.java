82abb5addf71cae7f29e71cc617f15964cf3d422
geekarist <christophe.pele@gmail.com>
2016-07-11 07:54:22 +0200

    Implement card RecyclerView (WIP)

--- 1-Base/src/main/java/com/example/android/materialdesigncodelab/CardContentFragment.java
+++ 1-Base/src/main/java/com/example/android/materialdesigncodelab/CardContentFragment.java
@@ -3,6 +3,8 @@ package com.example.android.materialdesigncodelab;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,7 +25,37 @@ public class CardContentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_card_content, container, false);
+        RecyclerView recyclerView = (RecyclerView) inflater.inflate(R.layout.recycler_view, container, false);
+        RecyclerView.Adapter adapter = new ContentAdapter();
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        return recyclerView;
     }
 
+    private class ContentAdapter extends RecyclerView.Adapter {
+
+        private static final int SIZE = 18;
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            View view = inflater.inflate(R.layout.fragment_card_content, parent, false);
+            return new ContentViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        }
+
+        @Override
+        public int getItemCount() {
+            return SIZE;
+        }
+    }
+
+    private class ContentViewHolder extends RecyclerView.ViewHolder {
+        public ContentViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
