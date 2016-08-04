d352b3d30488371dedca3b09074951086eb3d8b9
Christophe Pelé <christophe.pele@gmail.com>
2016-08-04 07:56:21 +0200

    Improve DetailActivity (WIP)

--- 1-Base/src/main/java/com/example/android/materialdesigncodelab/DetailActivity.java
+++ 1-Base/src/main/java/com/example/android/materialdesigncodelab/DetailActivity.java
@@ -1,14 +1,25 @@
 package com.example.android.materialdesigncodelab;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public class DetailActivity extends Activity {
+public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_detail);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        assert getSupportActionBar() != null;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        CollapsingToolbarLayout toolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        assert toolbarLayout != null;
+        toolbarLayout.setTitle(getString(R.string.item_title));
     }
 }
